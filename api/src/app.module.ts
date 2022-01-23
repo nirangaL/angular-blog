fe2b678371af85e13b688a181d1105abfaee01cd
niranga <nirangawh@hotmail.com>
2022-01-23 19:30:52 +0530

    Video-02: Create user feature module, with user service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,19 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3306,
       username: 'root',
       password: '',
       database: 'angular-blog',
+      autoLoadEntities: true,
       entities: [],
       synchronize: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ api/src/user/models/user.entity.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  username: string;
+}
